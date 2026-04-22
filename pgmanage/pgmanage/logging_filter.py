7ad51cbc3f175f21f0159d6041ca268778b8c41d
Eugene Dubinin <eugend@commandprompt.com>
2026-04-22 13:11:18 +0300

    filter out passwords from log lines which contain DB connection strings

--- pgmanage/pgmanage/logging_filter.py
+++ pgmanage/pgmanage/logging_filter.py
@@ -5,9 +5,13 @@ import re
 class MaskedDataFilter(logging.Filter):
     def filter(self, record):
         # TODO: add more data masking rules
-        rules = [('password":"\S*"', 'password":"[redacted]"')]
+        rules = [
+            (r'password":"\S*"', 'password":"[redacted]"'),
+            # mask passwords in connection strings
+            (r'([a-zA-Z0-9+.-]+://[^/\s:@]+:)([^@\s]+)(@[^?\s#]+)', r'\1[redacted]\3')
+        ]
 
-        if hasattr(record, "msg"):
+        if hasattr(record, "msg") and isinstance(record.msg, str):
             for old, new in rules:
                 record.msg = re.sub(old, new, record.msg)
 
